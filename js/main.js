44a0474e857608c1aa515cc4c7a3e1e6d2d18b24
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-10-31 10:03:43 -0700

    added functionality for mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(
-  //Dropdown menu functionality
+  //Dropdown header functionality
   $(window).scroll(function () {
     if ($(window).scrollTop() > $(window).height() / 3) {
       $(".dropDwnHeader").slideDown()
@@ -7,17 +7,25 @@ $(
       $(".dropDwnHeader").slideUp()
     }
   }),
+  //Menu functionality
+  $(".dropDwnHeader__mobile-menu--img").click(function () {
+    $(".dropDwnHeader__mobile-menu").slideToggle()
+  }),
+
+  $(".dropDwnHeader__mobile-menu--a").click(function () {
+    $(".dropDwnHeader__mobile-menu").slideToggle()
+  }),
   //Contact form functionality
   $("#submit").click(function () {
     var name = $("#name").val();
     var email = $("#email").val();
-    var email2= $("#email2").val();
+    var email2 = $("#email2").val();
     var message = $("#message").val();
     var recaptcha = $("#g-recaptcha-response").val();
     $(".contact__input").css("border", "1.5px solid rgb(20,171,155)")
     $(".contact_notification").html("<p>Send me a message!</p>")
 
-    if (name === "" || email === "" || email2 === "" ||message === "") {
+    if (name === "" || email === "" || email2 === "" || message === "") {
       $(".contact_notification").html("<p>Error! Please fill in all fields!</p>")
       $(".contact__input").css("border", "1.5px solid rgb(230, 0, 0)")
     } else if (email !== email2) {
@@ -34,11 +42,11 @@ $(
       }, function (data) {
         $(".contact_notification").html(data)
         $(".contact__loader").css("display", "none")
-        if (data ==="<p>Success! I'll be in touch shortly</p>"){
+        if (data === "<p>Success! I'll be in touch shortly</p>") {
           $(".contact__form")[0].reset()
         } else {
           $(".contact__input").css("border", "1.5px solid rgb(230, 0, 0)")
-        }  
+        }
       })
     }
   }),
